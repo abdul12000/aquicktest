5c11a40d987715f61e957e7c418df732272ef8cf
abdulsalam12000@yahoo.com <abdulsalam12000@yahoo.com>
2020-05-03 15:51:27 +0100

    added feature file

---
+++ src/main/java/com/quicktest/stepDef/VisaStepDef.java
@@ -0,0 +1,16 @@
+package com.quicktest.stepDef;
+
+
+import com.quicktest.base.BaseUtil;
+import io.cucumber.java8.En;
+
+public class VisaStepDef extends BaseUtil implements En {
+
+
+    public VisaStepDef() {
+        Given("^I provide a nationality of \"([^\"]*)\"$", (String arg0) -> {
+            // Write code here that turns the phrase above into concrete actions
+
+        });
+    }
+}
